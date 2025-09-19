33348a843d82d0961c003f9ad3573cb384ca3aaa
Ritik dangi <ritikdangi445566@gmail.com>
2025-09-20 00:48:05 +0530

    final some changes at home page

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("Anon Key:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0,10) + "...");
+
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
